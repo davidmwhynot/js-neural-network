db3863123ca0aa782e3260592fab89eafb4ccafe
David Whynot <davidmwhynot@gmail.com>
2019-08-12 21:31:24 -0400

    update massage scripts

---
+++ gen-output.sh
@@ -0,0 +1,24 @@
+cd data
+
+sed -n '12000001,13000000p;13000000q' bitstampUSD.csv > 13-000-000-bitstamp.csv
+sed -n '13000001,14000000p;14000000q' bitstampUSD.csv > 14-000-000-bitstamp.csv
+sed -n '14000001,15000000p;15000000q' bitstampUSD.csv > 15-000-000-bitstamp.csv
+sed -n '15000001,16000000p;16000000q' bitstampUSD.csv > 16-000-000-bitstamp.csv
+sed -n '16000001,17000000p;17000000q' bitstampUSD.csv > 17-000-000-bitstamp.csv
+sed -n '17000001,18000000p;18000000q' bitstampUSD.csv > 18-000-000-bitstamp.csv
+sed -n '18000001,19000000p;19000000q' bitstampUSD.csv > 19-000-000-bitstamp.csv
+sed -n '19000001,20000000p;20000000q' bitstampUSD.csv > 20-000-000-bitstamp.csv
+sed -n '20000001,21000000p;21000000q' bitstampUSD.csv > 21-000-000-bitstamp.csv
+sed -n '21000001,22000000p;22000000q' bitstampUSD.csv > 22-000-000-bitstamp.csv
+sed -n '22000001,23000000p;23000000q' bitstampUSD.csv > 23-000-000-bitstamp.csv
+sed -n '23000001,24000000p;24000000q' bitstampUSD.csv > 24-000-000-bitstamp.csv
+sed -n '24000001,25000000p;25000000q' bitstampUSD.csv > 25-000-000-bitstamp.csv
+sed -n '25000001,26000000p;26000000q' bitstampUSD.csv > 26-000-000-bitstamp.csv
+sed -n '26000001,27000000p;27000000q' bitstampUSD.csv > 27-000-000-bitstamp.csv
+sed -n '27000001,28000000p;28000000q' bitstampUSD.csv > 28-000-000-bitstamp.csv
+sed -n '28000001,29000000p;29000000q' bitstampUSD.csv > 29-000-000-bitstamp.csv
+sed -n '29000001,30000000p;30000000q' bitstampUSD.csv > 30-000-000-bitstamp.csv
+sed -n '30000001,31000000p;31000000q' bitstampUSD.csv > 31-000-000-bitstamp.csv
+sed -n '31000001,32000000p;32000000q' bitstampUSD.csv > 32-000-000-bitstamp.csv
+sed -n '32000001,33000000p;33000000q' bitstampUSD.csv > 33-000-000-bitstamp.csv
+sed -n '33000001,33595878p;33595878q' bitstampUSD.csv > 33-595-878-bitstamp.csv
--- src/massage.js
+++ src/massage.js
@@ -1,30 +1,47 @@
 const csv = require('csvtojson');
 
 async function main() {
-	const json = await csv({ noheader: true }).fromFile('data/tail1000.csv');
-	console.log('json', json);
+	const json = await csv({
+		noheader: true,
+		headers: ['time', 'price', 'amount']
+	}).fromFile('data/tail1000000.csv');
+	// console.log('json', json);
 
 	console.log('entries: ', json.length);
 
 	let sum = 0;
-	let a = Number(json[0].field1);
+	let min = 9999999;
+	let max = json[1].time - json[0].time;
+	let zeros = 0;
+
+	let a = Number(json[0].time);
 	let b = 0;
 
 	for (let i = 1; i < json.length; ++i) {
-		if (i % 1000 === 0) {
+		if (i % 100000 === 0) {
 			console.log('i', i);
 		}
-		// const date = new Date(Number(data.field1) * 1000);
+		// const date = new Date(Number(data.time) * 1000);
 		// console.log(date.toString());
 
 		const data = json[i];
 
-		b = Number(data.field1);
+		b = Number(data.time);
 		// console.log(b - a);
-		sum += b - a;
+		let difference = b - a;
+		sum += difference;
+
+		if (difference > max) max = difference;
+		if (difference < min && difference > 0) min = difference;
+		if (difference === 0) ++zeros;
+
 		a = b;
 	}
 	console.log('average', sum / (json.length - 1));
+	console.log('max', max);
+	console.log('min', min);
+	console.log('zeros', zeros);
 }
 
 main();
+// 1000000
